79e141ef68bb151726daf28dc3de71540a019ac5
RayanAnser <rayan.anser@hotmail.fr>
2023-05-23 15:20:11 +0200

    Exo 2

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,21 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
+
+  # Afficher toutes les taches
+  get '/tasks', to: 'tasks#index'
+  # Créer une tache
+  get '/tasks/new', to: 'tasks#new', as: 'new_task'
+  post '/tasks', to: 'tasks#create'
+
+  # Afficher une tache
+  get '/tasks/:id', to: 'tasks#show', as: 'task'
+
+  # Editer une tache
+  get '/tasks/:id/edit', to: 'tasks#edit', as: 'edit_task'
+  patch 'tasks/:id', to: 'tasks#update'
+
+  # Effacer une tache
+  get '/tasks/:id', to: 'tasks#destroy', as: 'destroy_task'
+  delete '/tasks/:id', to: 'tasks#destroy'
 end
